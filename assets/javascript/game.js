0aa6282b6958a8cbabb853b0a8354aeb444f1826
Sara Anspach <sara.anspach@gmail.com>
2018-08-25 15:10:32 -0400

    fifth

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,8 +11,6 @@ $(document).ready(function () {
         $("#random-number").text(randoNumber);
     });
 
-    var targetNumber = 36;
-    $("#number-to-guess").text(targetNumber);
     var counter = 0;
     var numberOptions = [10, 5, 3, 7];
 
@@ -46,11 +44,11 @@ $(document).ready(function () {
 
         alert("New score: " + counter);
 
-        if (counter === targetNumber) {
+        if (counter === randoNumber) {
             alert("You win!");
         }
 
-        else if (counter >= targetNumber) {
+        else if (counter >= randoNumber) {
             alert("You lose!!");
         }
     });
