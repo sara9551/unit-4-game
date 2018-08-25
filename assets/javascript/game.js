7197914af443e95c9360075bd5d93580d03ca29c
Sara Anspach <sara.anspach@gmail.com>
2018-08-25 14:22:53 -0400

    fourth

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,44 @@
-//$(document).ready(function () {
+$(document).ready(function () {
 
-    //var blueCrystalCounter = 0;
-    //var pinkCrystalCounter = 0;
-    //var greenCrystalCounter = 0;
+    $("#random-button").on("click", function () {
 
+        var randoNumber = "";
+        for (var i = 0; i < 1; i++) {
+            var random = Math.floor(Math.random() * 50) + 1;
 
-    $(document).ready(function () {
+            randoNumber = random + randoNumber;
+        }
+        $("#random-number").text(randoNumber);
+    });
 
-        $("#random-button").on("click", function () {
-
-            var randoNumber = "";
-            for (var i = 0; i < 1; i++) {
-                var random = Math.floor(Math.random() * 50) + 1;
-
-                randoNumber = random + randoNumber;
-
-            }
-            $("#random-number").text(randoNumber);
-
-        });
-
-    var targetNumber = 53;
-
+    var targetNumber = 36;
     $("#number-to-guess").text(targetNumber);
-
     var counter = 0;
-
     var numberOptions = [10, 5, 3, 7];
 
     for (var i = 0; i < numberOptions.length; i++) {
 
         var imageCrystal = $("<img>");
         imageCrystal.addClass("crystal-image");
-        imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+        imageCrystal.attr("src", "./assets/images/pinkcrystal.gif");
         imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-        $("#crystals").append(imageCrystal);
+        $("#crystals").html(imageCrystal);
+
+        var imageCrystal2 = $("<img>");
+        imageCrystal2.addClass("crystal-image");
+        imageCrystal2.attr("src", "./assets/images/bluecrystal.gif");
+        imageCrystal2.attr("data-crystalvalue", numberOptions[i]);
+        $("#crystals2").html(imageCrystal2);
+
+        var imageCrystal3 = $("<img>");
+        imageCrystal3.addClass("crystal-image");
+        imageCrystal3.attr("src", "./assets/images/greencrystal.gif");
+        imageCrystal3.attr("data-crystalvalue", numberOptions[i]);
+        $("#crystals3").html(imageCrystal3);
     }
 
     $(".crystal-image").on("click", function () {
-        
+
         var crystalValue = ($(this).attr("data-crystalvalue"));
         crystalValue = parseInt(crystalValue);
 
@@ -53,6 +53,5 @@
         else if (counter >= targetNumber) {
             alert("You lose!!");
         }
-
     });
 });
