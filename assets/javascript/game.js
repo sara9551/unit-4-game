ef1e6774c282d699ca2a182cd8db3868dc14f0aa
Sara Anspach <sara.anspach@gmail.com>
2018-08-25 16:06:43 -0400

    seventh

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
     });
 
     var counter = 0;
-    var numberOptions = [10, 5, 3, 7];
+    var numberOptions = [10, 5, 3, 7, 8, 21, 11, 4, 9];
 
     for (var i = 0; i < numberOptions.length; i++) {
 
