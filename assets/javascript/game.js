9b7cc8cd8e26ec0f307ba3d7951ef85003a46acf
Sara Anspach <sara.anspach@gmail.com>
2018-08-27 09:40:45 -0400

    had to make changes to my js file, so created new code so that numbers would be random

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,70 @@
-$(document).ready(function () {
+// //$(document).ready(function () {
 
-    $("#random-button").on("click", function () {
+//    // $("#random-button").on("click", function () {
 
-    var randoNumber = "";
-        for (var i = 0; i < 1; i++) {
-            var random = Math.floor(Math.random() * 50) + 1;
+//     //var randoNumber = "";
+//         for (var i = 0; i < 1; i++) {
+            
+//             var random = Math.floor(Math.random() * 50) + 1;
 
-            randoNumber = random + randoNumber;
-        }
-        $("#random-number").text(randoNumber);
-    });
+//             randoNumber = random + randoNumber;
+//         }
+//         $("#random-number").text(randoNumber);
+//     });
 
-    var randoNumber = "";
-    var counter = 0;
-    var numberOptions = [10, 5, 3, 7, 8, 21, 11, 4, 9];
-    var wins = 0
-    var loses = 0
+//     var randoNumber = "";
+//     var counter = 0;
+//     var numberOptions = [10, 5, 3, 7, 8, 21, 11, 4, 9];
+//     var array = [];
+//     var total = [];
+//     var player = 0;
 
-    for (var i = 0; i < numberOptions.length; i++) {
+//     for (var i = 0; i < numberOptions.length; i++) {
 
-        var imageCrystal = $("<img>");
-        imageCrystal.addClass("crystal-image");
-        imageCrystal.attr("src", "./assets/images/pinkcrystal.gif");
-        imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-        $("#crystals").html(imageCrystal);
+//         var imageCrystal = $("<img>");
+//         imageCrystal.addClass("crystal-image");
+//         imageCrystal.attr("src", "./assets/images/pinkcrystal.gif");
+//         imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+//         $("#crystals").html(imageCrystal);
 
-        var imageCrystal2 = $("<img>");
-        imageCrystal2.addClass("crystal-image");
-        imageCrystal2.attr("src", "./assets/images/bluecrystal.gif");
-        imageCrystal2.attr("data-crystalvalue", numberOptions[i]);
-        $("#crystals2").html(imageCrystal2);
+//         var imageCrystal2 = $("<img>");
+//         imageCrystal2.addClass("crystal-image");
+//         imageCrystal2.attr("src", "./assets/images/bluecrystal.gif");
+//         imageCrystal2.attr("data-crystalvalue", numberOptions[i]);
+//         $("#crystals2").html(imageCrystal2);
 
-        var imageCrystal3 = $("<img>");
-        imageCrystal3.addClass("crystal-image");
-        imageCrystal3.attr("src", "./assets/images/greencrystal.gif");
-        imageCrystal3.attr("data-crystalvalue", numberOptions[i]);
-        $("#crystals3").html(imageCrystal3);
-    }
+//         var imageCrystal3 = $("<img>");
+//         imageCrystal3.addClass("crystal-image");
+//         imageCrystal3.attr("src", "./assets/images/greencrystal.gif");
+//         imageCrystal3.attr("data-crystalvalue", numberOptions[i]);
+//         $("#crystals3").html(imageCrystal3);
+//     }
 
-    $(".crystal-image").on("click", function () {
+//     $(".crystal-image").on("click", function () {
+//     if ($(imageCrystal).attr("class") == "imageCrystal") {
+//         total.push(array[i]);
+//      }
 
-        var crystalValue = ($(this).attr("data-crystalvalue"));
-        crystalValue = parseInt(crystalValue);
+//      if ($(imageCrystal2).attr("class") == "imageCrystal2") {
+//         total.push(array[i]);
+//      }
 
-        counter += crystalValue;
+//      if ($(imageCrystal3).attr("class") == "imageCrystal3") {
+//         total.push(array[i]);
+//      }
+//     });
 
-        alert("New score: " + counter);
+//     $(".crystal-image").on("click", function () {
 
-        if (counter === randoNumber) {
-            $("h4").append("You win");
-        }
+//         var crystalValue = ($(this).attr("data-crystalvalue"));
+//         crystalValue = sum + parseInt(crystalValue);
 
-        else if (counter >= randoNumber) {
-            $("h4").append("You loose");
-        }
-    });
-})
+//         if (sum === randoNumber) {
+//          win++;
+//             $("h4").append("You win");
+//         }
+
+//         else if (counter >= randoNumber) {
+//             $("h4").append("You loose");
+//         }
+//     });
